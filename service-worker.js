fde1d92fb656115a8f922c6eb40ac8bd1dea1854
chenkai0520 <chenkai0520@users.noreply.github.com>
2021-09-02 09:46:09 +0000

    deploy: ab92626101d2369c8835082814940a8abca708e5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'ck-blog-cache-v3';
+const cacheName = 'ck-blog-cache-v4';
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(cacheName).then((cache) => cache.addAll([
@@ -6,8 +6,8 @@ self.addEventListener('install', (e) => {
       '/js/custom.js',
       '/js/cusdis/cusdis.min.js',
       '/js/cusdis/iframe.umd.js',
-      '/images/favicon-16x16.png',
       '/images/favicon-32x32.png',
+      '/images/favicon-192x192.png',
       '/images/favicon-512x512.png',
       '/manifest.webmanifest'
     ])),
