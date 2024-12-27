afd5a1deb2dd92a8497a1116f65aa18fe6569063
ckvv <k_0520@qq.com>
2024-12-27 11:04:35 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ckvv.github.io',
+  site: 'https://chenkai.life',
   integrations: [mdx(), sitemap(), vue(), tailwind()],
   devToolbar: {
     enabled: false,
