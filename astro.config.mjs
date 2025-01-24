787c73aa3e30e16873c44c48c110294e05e68e02
ckvv <k_0520@qq.com>
2025-01-24 10:20:32 +0800

    fix: tailwindcss 部分页面样式失效 先回退到 v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
-import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ckvv.net',
   vite: {
-    plugins: [tailwindcss()],
     server: {
       hmr: {
         overlay: false,
       },
     },
   },
-  integrations: [mdx(), vue()],
+  integrations: [mdx(), vue(), tailwind()],
   devToolbar: {
     enabled: false,
   },
