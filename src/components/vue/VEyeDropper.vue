09f4d7829cba71ff7ad77b05d0014e39dd1a2ce6
陈锴 <k_0520@qq.com>
2025-02-13 14:50:25 +0800

    添加浏览器屏幕颜色吸取

---
+++ src/components/vue/VEyeDropper.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const color = ref();
+const error = ref();
+
+function handlerCLick() {
+  const eyeDropper = new (window as any).EyeDropper();
+  eyeDropper
+    .open()
+    .then((res: any) => {
+      color.value = res;
+    })
+    .catch((e: any) => {
+      error.value = e;
+    });
+}
+</script>
+
+<template>
+  <div @click="handlerCLick">
+    <slot />
+  </div>
+  {{ color }}
+  {{ error }}
+</template>
